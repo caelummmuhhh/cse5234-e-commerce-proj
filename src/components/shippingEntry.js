29c32f0145af7ebc9700ab5ebf621518bb57a0f5
Ryan Tussing <ryan.m.tussing@gmail.com>
2024-10-02 18:22:19 -0400

    Adding components folder

---
+++ src/components/shippingEntry.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ShippingEntry = () => {
+    let TITLE = 'Shipping Entry Page';
+
+    return (
+        <div>
+            <h1>
+                {TITLE}
+            </h1>
+        </div>
+    );
+};
+
+export default ShippingEntry;
