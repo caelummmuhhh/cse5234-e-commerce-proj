799d7a15f68d8de8fc26e363c23d8c7483fee3cf
RealEthanTsou <ethantsou@gmail.com>
2024-10-02 18:39:57 -0400

    purchasePage

--- src/components/purchase.js
+++ src/components/purchase.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React from "react";
+import {useState} from "react";
+import {useNavigate} from 'react-router-dom'
 
 const Purchase = () => {
+    const [order, setOrder] = useState({
+        buyQuantity: [0,0,0,0,0], credit_card_number: '', expir_date:'', cvvCode:'', card_holder_name: '',
+        address_1:'',  address_2:'', city:'', state:'', zip:'',
+    })
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        navigate('/purchase/paymentEntry', {order: order, setOrder: setOrder});
+    }
+
+    console.log('order: ', order);
+
     let TITLE = 'Purchase Page';
 
     return (
         <div>
-            <h1>
-                {TITLE}
-            </h1>
+            <form onSubmit={handleSubmit}>
+            <label>Product 1</label>
+            <input
+                type="number"
+                required
+                onChange={(e) =>
+                {order.buyQuantity[0] = e.target.value;}}
+                />
+            <br/>
+            <label>Product 2</label>
+            <input
+                type="number"
+                required
+                onChange={(e) =>
+                {order.buyQuantity[1] = e.target.value;}}
+                />
+            <br/>
+            <button className='button'>Pay</button>
+            </form>
         </div>
     );
 };
