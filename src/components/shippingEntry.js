6882f406f0378f0ab14c34d859df91a676e8e692
RealEthanTsou <ethantsou@gmail.com>
2024-10-03 15:01:33 -0400

    shippingentry

--- src/components/shippingEntry.js
+++ src/components/shippingEntry.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ShippingEntry = () => {
-    let TITLE = 'Shipping Entry Page';
+    const [shippingDetails, setShippingDetails] = useState({
+        address_1: '',
+        address_2: '',
+        city: '',
+        state: '',
+        zip: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Shipping Details: ', shippingDetails);
+
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setShippingDetails((prevDetails) => ({
+            ...prevDetails,
+            [name]: value,
+        }));
+    };
 
     return (
         <div>
-            <h1>
-                {TITLE}
-            </h1>
+            <h2>Shipping Information</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Address 1:
+                    <input
+                        type="text"
+                        name="address_1"
+                        value={shippingDetails.address_1}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <br/>
+                <label>
+                    Address 2:
+                    <input
+                        type="text"
+                        name="address_2"
+                        value={shippingDetails.address_2}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br/>
+                <label>
+                    City:
+                    <input
+                        type="text"
+                        name="city"
+                        value={shippingDetails.city}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <br/>
+                <label>
+                    State:
+                    <input
+                        type="text"
+                        name="state"
+                        value={shippingDetails.state}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <br/>
+                <label>
+                    Zip Code:
+                    <input
+                        type="text"
+                        name="zip"
+                        value={shippingDetails.zip}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
