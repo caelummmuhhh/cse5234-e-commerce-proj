9970ce50ec8acd0dee46abf031a9d059a6e7a3ab
caelummmuhhh <115360461+caelummmuhhh@users.noreply.github.com>
2024-12-02 16:42:37 -0500

    add image to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,9 @@ const Home = () => {
     return (
         <div className="home">
             <div className="home-content">
-                <h1>Welcome to Toaster City!</h1>
-                <p>We hold the world's largest supply of Toasters in the world!</p>
+                <h1 style={{color: "#c9c9c9"}}>Welcome to Toaster City!</h1>
+                <p style={{color: "#adadad"}}>We hold the world's largest supply of Toasters in the world!</p>
+                <img className="display-img" alt="" src={`${process.env.PUBLIC_URL}/images/display_toaster.png`}></img>
             </div>
         </div>
     );
