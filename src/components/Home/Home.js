837a426be691043b26404b99d9bc3269fd998458
caelummmuhhh <115360461+caelummmuhhh@users.noreply.github.com>
2024-12-02 17:13:27 -0500

    add more in front

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="home">
             <div className="home-content">
                 <h1 style={{color: "#c9c9c9"}}>Welcome to Toaster City!</h1>
-                <p style={{color: "#adadad"}}>We hold the world's largest supply of Toasters in the world!</p>
+                <p style={{color: "#adadad"}}>We hold the world's largest supply of Toasters in the world <br></br>designed tech and culinary expoerts!</p>
                 <img className="display-img" alt="" src={`${process.env.PUBLIC_URL}/images/display_toaster.png`}></img>
             </div>
         </div>
